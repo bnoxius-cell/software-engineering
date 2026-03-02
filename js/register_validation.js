2f0d4086f95e5d4b40c1691550e57f0f1610d060
bnoxius-cell <idevadrian@gmail.com>
2026-03-02 17:54:07 +0800

    finished fixing admin pages structure

---
+++ js/register_validation.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const fullNameInput = document.getElementById('fullName');
+const emailInput = document.getElementById('email');
+const roleInput = document.getElementById('role');
+const passwordInput = document.getElementById('password');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let errors = []
+})
