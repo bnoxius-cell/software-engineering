aa3c7b586761a364587e2d3b474b4cafc2758c88
bnoxius-cell <idevadrian@gmail.com>
2026-03-02 08:37:12 +0800

    admin pages clean up

--- js/include.js
+++ js/include.js
@@ -38,3 +38,9 @@ fetch("../pages/components/footer.html")
   .then(html => { document.getElementById("footer").innerHTML = html; 
   }) 
   .catch(err => console.error("Footer load failed", err));
+  
+fetch("../components/sidebar.html") 
+  .then(response => response.text()) 
+  .then(html => { document.getElementById("sidebar").innerHTML = html; 
+  }) 
+  .catch(err => console.error("Sidebar load failed", err));
