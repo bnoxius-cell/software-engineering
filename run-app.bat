c2af287a273a03e6495cbd3ba9f61588391d29d3
Tony <anadventurer100@gmail.com>
2026-03-11 00:43:50 +0800

    added run-app.bat

---
+++ run-app.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d C:\Users\acm\software-engineering
+
+echo ====================================
+echo   EMC Artisan E-Portfolio Server
+echo ====================================
+echo.
+echo Starting the Node.js server...
+echo Server will be available at: http://localhost:5000
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+npm run dev
