920886f6b6bf738e15068cb487e06b6c0e85a13b
bnoxius-cell <idevadrian@gmail.com>
2026-03-10 22:24:43 +0800

    finished the back-end for authentication.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,21 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-export const protect = async (req, res, next) => {
+const protect = async (req, res, next) => {
     let token;
-
-    if (f)
+    
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+            return next();
+        } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: "Not authorized, token failed" });
+        }
+    }
+    return res.status(401).json({ message: "Not authorized, no token" });
 }
+
+module.exports = { protect };
