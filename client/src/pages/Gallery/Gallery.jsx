1893056fd8b2eb6b87bb951b28d55a4483b1191c
bnoxius-cell <idevadrian@gmail.com>
2026-03-13 19:58:04 +0800

    finished with gallery page. TODO: filter

--- client/src/pages/Gallery/Gallery.jsx
+++ client/src/pages/Gallery/Gallery.jsx
@@ -35,6 +35,7 @@ const Gallery = () => {
                   <h3>{art.title}</h3>
                   <p>by {art.artistName}</p>
                   <p>Genre: {art.genre}</p>
+                  <p>{art.createdAt}</p>
                 </div>
               </div>
             ))}
