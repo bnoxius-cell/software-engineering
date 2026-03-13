09353ee7cc596aa3ddfe4c9e601aec7307cc6639
bnoxius-cell <idevadrian@gmail.com>
2026-03-13 19:58:04 +0800

    finished with gallery page. TODO: filter

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Artwork = require("../models/Uploads");
 
+// GET all artworks
+router.get("/", async (req, res) => {
+  try {
+    const artworks = await Artwork.find({});
+    res.status(200).json(artworks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// POST create new artwork
 router.post("/create", async (req, res) => {
   try {
     const newArtwork = await Artwork.create(req.body);
